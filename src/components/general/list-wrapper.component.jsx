7acbd4c2a2ed57de7d08f51528e40eca6d52b5c3
vardan <vardan_live@live.com>
2019-11-11 18:39:58 +0400

    feat: add new records manually

--- src/components/general/list-wrapper.component.jsx
+++ src/components/general/list-wrapper.component.jsx
@@ -12,50 +12,6 @@ const useStyles = makeStyles({
     height: 'calc(100vh - 55px)',
     overflow: 'hidden',
   },
-  wrapperWithMovingContents: {
-    // position: 'absolute',
-    /*'-webkitAnimation': 'mover 1s infinite  alternate',
-    animation: 'mover 1s infinite  alternate',*/
-    /*'::@-webkit-keyframes mover': `
-      {0% { transform: translateY(0); }
-      100% { transform: translateY(-10px); }`,
-
-    '::@keyframes mover': `
-      {0% { transform: translateY(0); }
-      100% { transform: translateY(-10px); }`,*/
-    /*'@-webkit-keyframes mover': {
-      '0%': {
-        transform: 'translateY(0)',
-      },
-      '100%': {
-        transform: 'translateY(-10px)',
-      },
-    },
-    '@keyframes mover': {
-      '0%': {
-        transform: 'translateY(0)',
-      },
-      '100%': {
-        transform: 'translateY(-10px)',
-      },
-    },*/
-    /*'@-webkit-keyframes mover': `{
-      '0%': {
-        transform: 'translateY(0)',
-      },
-      '100%': {
-        transform: 'translateY(-10px)',
-      },
-    }`,*/
-    /*'::@keyframes mover': {
-      '0%': {
-        transform: 'translateY(0)',
-      },
-      '100%': {
-        transform: 'translateY(-10px)',
-      },
-    },*/
-  },
 });
 
 const ListWrapperComponent = () => {
--- src/components/general/main-wrapper.component.jsx
+++ src/components/general/main-wrapper.component.jsx
@@ -1,3 +1,5 @@
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import { node } from 'prop-types';
 import React, { createContext, useEffect, useState } from 'react';
 
@@ -18,34 +20,57 @@ const MainWrapperComponent = ({ children }) => {
         { id: 5, title: 'donator 5', amount: '800', currency: '$' },
         { id: 6, title: 'donator 6', amount: '400000', currency: 'AMD' },
         { id: 7, title: 'donator 7', amount: '1500', currency: '$' },
-        { id: 8, title: 'donator 8', amount: '800', currency: '$' },
-        { id: 9, title: 'donator 9', amount: '400000', currency: 'AMD' },
-        { id: 10, title: 'donator 10', amount: '1500', currency: '$' },
-        { id: 11, title: 'donator 11', amount: '800', currency: '$' },
-        { id: 12, title: 'donator 12', amount: '400000', currency: 'AMD' },
-        { id: 13, title: 'donator 13', amount: '1500', currency: '$' },
-        { id: 14, title: 'donator 14', amount: '800', currency: '$' },
-        { id: 15, title: 'donator 15', amount: '400000', currency: 'AMD' },
-        { id: 16, title: 'donator 16', amount: '1500', currency: '$' },
-        { id: 17, title: 'donator 17', amount: '800', currency: '$' },
-        { id: 18, title: 'donator 18', amount: '400000', currency: 'AMD' },
-        { id: 19, title: 'donator 19', amount: '1500', currency: '$' },
-        { id: 20, title: 'donator 20', amount: '800', currency: '$' },
-        { id: 21, title: 'donator 21', amount: '400000', currency: 'AMD' },
-        { id: 22, title: 'donator 22', amount: '1500', currency: '$' },
-        { id: 23, title: 'donator 23', amount: '800', currency: '$' },
-        { id: 24, title: 'donator 24', amount: '400000', currency: 'AMD' },
+        // { id: 8, title: 'donator 8', amount: '800', currency: '$' },
+        // { id: 9, title: 'donator 9', amount: '400000', currency: 'AMD' },
+        // { id: 10, title: 'donator 10', amount: '1500', currency: '$' },
+        // { id: 11, title: 'donator 11', amount: '800', currency: '$' },
+        // { id: 12, title: 'donator 12', amount: '400000', currency: 'AMD' },
+        // { id: 13, title: 'donator 13', amount: '1500', currency: '$' },
+        // { id: 14, title: 'donator 14', amount: '800', currency: '$' },
+        // { id: 15, title: 'donator 15', amount: '400000', currency: 'AMD' },
+        // { id: 16, title: 'donator 16', amount: '1500', currency: '$' },
+        // { id: 17, title: 'donator 17', amount: '800', currency: '$' },
+        // { id: 18, title: 'donator 18', amount: '400000', currency: 'AMD' },
+        // { id: 19, title: 'donator 19', amount: '1500', currency: '$' },
+        // { id: 20, title: 'donator 20', amount: '800', currency: '$' },
+        // { id: 21, title: 'donator 21', amount: '400000', currency: 'AMD' },
+        // { id: 22, title: 'donator 22', amount: '1500', currency: '$' },
+        // { id: 23, title: 'donator 23', amount: '800', currency: '$' },
+        // { id: 24, title: 'donator 24', amount: '400000', currency: 'AMD' },
       ]);
     });
   }, []);
 
+  const addData = () => {
+    // data.push();
+    // console.log('here');
+    const nextItemId = donationsList.length + 1;
+    setDonationsList([
+      ...donationsList,
+      {
+        id: nextItemId,
+        title: `donator ${nextItemId}`,
+        amount: (Math.random() * 1000).toFixed(),
+        currency: '$',
+      },
+    ]);
+    // console.log({ donationsList });
+  };
+
   return (
-    <div>
-      <div>Left part (logos)</div>
-      <DonationsListContext.Provider value={donationsList}>
-        {children}
-      </DonationsListContext.Provider>
-    </div>
+    <Grid container direction="row" justify="center" alignItems="center">
+      <Grid item xs={6}>
+        <div>Left part (logos)</div>
+        <Button variant="contained" onClick={addData}>
+          Add new records
+        </Button>
+      </Grid>
+      <Grid item xs={6}>
+        <DonationsListContext.Provider value={donationsList}>
+          {children}
+        </DonationsListContext.Provider>
+      </Grid>
+    </Grid>
   );
 };
 
